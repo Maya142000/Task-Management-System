7a47a9543a6e14c99715cbb1416b3d9b40662ed5
Mahesh Sharad Mohite <mohitemahesh006@gmail.com>
2024-12-08 00:31:59 +0530

    Handle the validation of user access and protect sensitive routes in your application.

---
+++ Middlewares/UserAuth.js
@@ -0,0 +1,87 @@
+const UserModel = require("../controllers/Admin/UserModel")
+const {emailValidator} = require("../Utils/EmailValidator")
+const {PasswordValidator} = require("../Utils/PasswordValidator")
+const bcrypt = require("bcryptjs");
+
+module.exports = { 
+    authRegister :  async (req,res,next) => {
+        try {
+            const NewData = req.body
+
+            if (!NewData.FirstName) {
+                res.send({ status : false, message : "Please provide your first Name..!" })
+            }
+
+            if (!NewData.LastName) {
+                res.send({ status : false, message : "Please provide your Last Name..!" })
+            }
+
+            if (!NewData.Email) {
+                res.send({ status : false, message : "Please enter your Email..!" })
+            }
+
+            if (!NewData.OTP) {
+                return res.send({ status : false, message : "Please enter your OTP..!" })
+            }
+
+            if (!NewData.Password) {
+                return res.send({ status : false, message : "Please enter your Password..!" })
+            }
+
+            try {
+
+                emailValidator(NewData.Email);
+                PasswordValidator(NewData.Password);
+
+            } catch (error) {
+                return res.send({
+                    status : 400,
+                    success: false,
+                    error : error.message
+                })
+            }
+            next();
+            
+        } catch (error) {
+            if (!res.headersSent) {
+                return res.status(500).json({
+                    status : false,
+                    message : "Error in try",
+                    error : error.message
+                })
+            }
+        }
+    },
+
+
+
+    authlogin : async (req, res, next) => {
+        try {
+            const { Email, Password } = req.body;
+            
+            if (!Email || !Password) return res.send({ status: 400, success: false, message: "Please provide your Email and Password to proceed..!" });
+
+            if (!Email) return res.send({ status: 400, success: false, message: "Please enter your Email..!" });
+            if (!Password) return res.send({ status: 400, success: false, message: "Please enter your Password..!" });
+            
+            const existingUser = await UserModel.findOne({ Email: Email }).exec();
+            if (!existingUser) return res.send({ status: 401, success: false, message: "To continue, please sign up first." });
+            
+            const PasswordMatch = await bcrypt.compare(Password, existingUser.Password);
+
+            if (PasswordMatch) {
+                next();
+            } else {
+                return res.send({  status: 401,  success: false,  message: "Oops..!, Wrong Password." });
+            }
+        } catch (error) {
+            res.send({ 
+                status: false, 
+                message: 'Error in Try', 
+                error: error.message 
+            });
+        }
+    }
+    
+
+};
